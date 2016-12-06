7a0c85f2ccce455587d093f8dbeb1450c7a8d889
tanysaur <tanysaur@gmail.com>
2016-12-05 23:06:54 -0600

    added basic html, css and js

---
+++ app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD5XYZHecNShIOC9c69lKZgIVPYyHyysF8",
+    authDomain: "project-allergi.firebaseapp.com",
+    databaseURL: "https://project-allergi.firebaseio.com",
+    storageBucket: "project-allergi.appspot.com",
+    messagingSenderId: "149119827295"
+  };
+  firebase.initializeApp(config);

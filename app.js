561c2db9852bc5a5d8e74ae95ac8e4c88fc1938d
tanysaur <tanysaur@gmail.com>
2016-12-11 21:46:30 -0600

    Added icons to the page

--- app.js
+++ app.js
@@ -12,17 +12,17 @@
 
   //LabelAPI key: x49rczhgarkyz7hzpxsez2nn
   var labelAPIkey = "x49rczhgarkyz7hzpxsez2nn";
-  var allergens = ["corn", "shellfish", "egg", "milk", "peanuts"];
+  var walmartAPIkey = "2ahdxqdhzd7thd5u846wgf2m";
+  var allergens = ["Cereals", "Shellfish", "Egg", "Fish", "Milk", "Peanuts","Sulfites","Tree Nuts",
+                    "Soybean","Sesame Seeds","Gluten","Lactose","Corn","Wheat","Coconut"];
   var searchInput = '';
   var upc = [];
   var labelURL = ''; 
   var sid = '';
 
   $(document).on("click", "#add-product", function() {
-    console.log(sid);
     searchInput = $("#productInput").val().trim();
-    console.log("My SI: " + searchInput);
-    labelURL = "http://cors-anywhere.herokuapp.com/http://api.foodessentials.com/searchprods?q=" + searchInput + "&sid=" + "ad257332-9bf5-4866-997c-9847dbd29f07" + "&n=10&s=0&f=json&api_key=" + labelAPIkey;
+    searchURL = "http://cors-anywhere.herokuapp.com/http://api.foodessentials.com/searchprods?q=" + searchInput + "&sid=" + "ad257332-9bf5-4866-997c-9847dbd29f07" + "&n=20&s=0&f=json&api_key=" + labelAPIkey;
 
     // Code for handling the push
     database.ref().push({
@@ -32,18 +32,43 @@
     $("#productInput").val("");
 
     $.ajax({
-        url: labelURL,    
+        url: searchURL,    
         method: 'GET'
     }).done(function(response) {
       // searchInput = $("#productInput").val().trim();
-      console.log(response);
-      for(i = 0; i < 10; i++){
+      $("#new-input").empty();
+      for(i = 0; i < 20; i++){
         console.log("Name: " + response.productsArray[i].product_name);
         console.log("Description: " + response.productsArray[i].product_description);
         console.log("UPC: " + response.productsArray[i].upc);
+        $("#new-input").append("<tr><td>" + response.productsArray[i].product_name + "</td><td>" + response.productsArray[i].upc);
       }   
     });
   });
+
+  $(document).on("click", "#add-upc", function(){
+    upcInput = $("#upcInput").val().trim();
+    upcURL = "http://cors-anywhere.herokuapp.com/http://api.foodessentials.com/label?u=" + upcInput + "&sid=" + "3bf8bb6f-99d8-42f6-a422-3a8c37a10de8" + "&appid=demoApp_01&f=json" + "&api_key=" + labelAPIkey;
+    database.ref().push({
+      upcInput: upcInput
+    });
+
+    $("#upcInput").val("");
+
+    $.ajax({
+      url: upcURL,
+      method: 'GET'
+    }).done(function(upcresponse) {
+      $("#new-input").empty();
+      for(i = 0; i < 15; i++){
+        $("#new-UPCinput").append(
+          "<div class='col-md-12'>" + upcresponse.allergens[i].allergen_name + "<br>"  + "Allergen value: "
+          + upcresponse.allergens[i].allergen_value + "<br>" + " Red: " + upcresponse.allergens[i].allergen_red_ingredients  
+          + "<br>" +  " Yellow: " + upcresponse.allergens[i].allergen_yellow_ingredients + "<br><br>");
+      } 
+    })
+  });
+
 
   //Render allergen buttons
   function renderButtons(){ 
@@ -52,7 +77,7 @@
   
     // Loops through the array of allergens
     for (var i = 0; i < allergens.length; i++){   
-        $("#buttons-appear-here").append("<button class='allergensButton' data-button='" + allergens[i] +"'>" + allergens[i] + "</button>");
+      $("#buttons-appear-here").append("<button class='allergensButton' data-button='" + allergens[i] +"'>" + allergens[i] + "</button> ");
     }
   };
   
@@ -76,4 +101,4 @@
   });
 
   // ========================================================
-  renderButtons();
+  //renderButtons();
